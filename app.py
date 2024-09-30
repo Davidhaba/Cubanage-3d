13edff14550c76e7df93debd5eace9a4eec68276
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2024-10-01 00:37:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import logging
 import os
